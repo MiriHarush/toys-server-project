7e2ca13228f2432cbc74d495ae7a964849396d03
MiriHarush <132377898+MiriHarush@users.noreply.github.com>
2023-12-04 14:35:30 +0200

    express

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-aoo.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,"public")))
 
 app.use("/users", userRoutes);
 app.use("/toys", toysRoutes);
